000cede501f4cca5af3eeeb95635ec2654b17211
j.ren <j.ren@alphabets.cn>
2024-04-11 12:56:53 +0800

    init: add i18next react-i18next

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useTranslation } from './locale'
 
 function App() {
   const [count, setCount] = useState(0)
+  const { t } = useTranslation('首页');
 
   return (
     <>
@@ -28,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      {t('测试')}
     </>
   )
 }
---
+++ src/locale/index.ts
@@ -0,0 +1,53 @@
+import i18next, { Resource } from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import common from "./trans/common.json";
+import 首页 from "./trans/首页.json";
+
+type Lng = "en" | "zh";
+type Entry = Record<Lng, string>;
+type Translation = Record<string, Entry>;
+
+type _Resource = Record<
+  Lng,
+  {
+    [ns: string]: {
+      [key: string]: string;
+    };
+  }
+>;
+
+function formatTranslation(trans: Record<string, Translation>): Resource {
+  const resources: _Resource = {
+    en: {},
+    zh: {},
+  };
+
+  for (const [ns, nsTrans] of Object.entries(trans)) {
+    resources.en[ns] = {};
+    resources.zh[ns] = {};
+    for (const [key, entry] of Object.entries(nsTrans)) {
+      const { en, zh } = entry;
+      resources.en[ns][key] = en;
+      resources.zh[ns][key] = zh;
+    }
+  }
+
+  return resources;
+}
+
+export function init() {
+  const resources = formatTranslation({ common, 首页 });
+  console.log(resources);
+  i18next
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+      lng: "zh",
+      fallbackLng: "zh",
+      ns: ["common", "首页"],
+      defaultNS: "common",
+      resources: resources,
+    });
+}
+
+export { useTranslation } from "react-i18next";
