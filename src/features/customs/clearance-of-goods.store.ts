a187a940524dbeaa46d5b05a6c9b5af3e46e901e
j.ren <j.ren@alphabets.cn>
2024-05-10 17:36:32 +0800

    wip: download

--- src/features/customs/clearance-of-goods.store.ts
+++ src/features/customs/clearance-of-goods.store.ts
@@ -15,21 +15,18 @@ export class ClearanceOfGoodsStore {
   async export(queryParams: CustomITemsQueryParams = {}) {
     // await net.post('/api/customsItem/export', queryParams, { timeout: 60 * 1000 * 5, responseType: 'blob',maxRedirects: 0 });
     const url = 'http://customs-service.oss-us-west-1.aliyuncs.com/dev%2Ftemp%2Fd15uo294dyi2fydckm.xlsx?Expires=1715336230&OSSAccessKeyId=LTAI5tK8ZuA8SVmV7LW2jb11&Signature=UKswyTtzEb9aU5MHOzyrkzIfR0Y%3D&response-content-disposition=attachment%3B%20filename%3D%22customs_item_export_1715332628.xlsx%22';
-    const blob = await axios.get(url, { responseType: 'blob', headers: {
+    const res = await axios.get(url, { responseType: 'blob', headers: {
       'Access-Control-Allow-Origin': '*',
     } });
 
-    const fileUrl = window.URL.createObjectURL(blob);
-  
+    const blob = res.data;
+    console.log(typeof blob)
+    if (!(blob instanceof Blob)) throw new Error('blob');
+
     const link = document.createElement('a');
-    link.href = fileUrl;
-  
-    link.setAttribute('download', `customs_item_export_1715332628.xlsx`);
-    document.body.appendChild(link);
-  
+    link.href = window.URL.createObjectURL(blob);
+    link.download = 'customs_item_export_1715332628.xlsx';
     link.click();
-    window.URL.revokeObjectURL(fileUrl);
-    link.parentNode?.removeChild(link);
-    
+    window.URL.revokeObjectURL(link.href);
   }
 }
