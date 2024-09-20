0872a953a2328532dfa04a63517d757310a7c43d
j.ren <j.ren@alphabets.cn>
2024-09-20 12:15:53 +0800

    wip: 【客户信息】新增筛选和导出

--- src/features/customs/RM/customer-info-list.component.tsx
+++ src/features/customs/RM/customer-info-list.component.tsx
@@ -1,10 +1,12 @@
 import {
+  Button,
   ClientGrid,
   Col,
   Container,
   FilterContainer,
   Form,
   Input,
+  Row,
   Table,
 } from "@components";
 import { observer } from "mobx-react-lite";
@@ -13,6 +15,7 @@ import styles from "./customer-info-list.module.less";
 import { useCallback, useMemo } from "react";
 import { useStore } from "@hooks";
 import { CustomerInfoListStore } from "./customer-info-list.store";
+import { CloudDownloadOutlined } from "@ant-design/icons";
 
 function TemplateListComponent() {
   const { store, t } = useStore(CustomerInfoListStore)();
@@ -21,8 +24,15 @@ function TemplateListComponent() {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleFinish = useCallback((values: any = {}) => {
-    const { productName, productNameCn, exportHsCode, importHsCode } = values;
+    const {
+      customerName,
+      productName,
+      productNameCn,
+      exportHsCode,
+      importHsCode,
+    } = values;
     gridStore.setQueryParams({
+      customerName: customerName || undefined,
       productName: productName || undefined,
       productNameCn: productNameCn || undefined,
       exportHsCode: exportHsCode || undefined,
@@ -33,6 +43,11 @@ function TemplateListComponent() {
   return (
     <Container className={styles.container} loading={store.loading}>
       <FilterContainer onFinish={handleFinish}>
+        <Col span={6}>
+          <Form.Item name="customerName" label={t("客户名称")}>
+            <Input />
+          </Form.Item>
+        </Col>
         <Col span={6}>
           <Form.Item name="productName" label={t("英文品名")}>
             <Input />
@@ -55,6 +70,15 @@ function TemplateListComponent() {
         </Col>
       </FilterContainer>
       <Container title={t("客户信息")} wrapperClassName={styles.wrapper} table>
+        <Row justify="end" style={{ padding: "0 10px" }}>
+          <Button
+            className="operation-btn mr-4 mb-4"
+            icon={<CloudDownloadOutlined />}
+            onClick={store.export.bind(store, gridStore.queryParams as any)}
+          >
+            {t("导出已筛选数据")}
+          </Button>
+        </Row>
         <Table
           widthFit
           bordered
